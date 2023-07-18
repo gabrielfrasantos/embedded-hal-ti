b6f00c6ff06c2945884d8ba9626b62809073cafd
Gabriel Santos <gabriel.fra.santos@gmail.com>
2023-07-18 09:40:06 +0200

    Unique ID added

--- hal_tiva/tiva/UniqueDeviceId.cpp
+++ hal_tiva/tiva/UniqueDeviceId.cpp
@@ -2,35 +2,9 @@
 
 namespace hal
 {
-#if defined(STM32F373xC)
     infra::ConstByteRange UniqueDeviceId()
     {
-        const uint8_t* base = reinterpret_cast<const uint8_t*>(0x1FFFF7AC);
-        return infra::ConstByteRange(base, base + 12);
+        const uint8_t* base = reinterpret_cast<const uint8_t*>(0x400FE000);
+        return infra::ConstByteRange(base, base + 8);
     }
-#endif
-
-#if defined(STM32F407xx)
-    infra::ConstByteRange UniqueDeviceId()
-    {
-        const uint8_t* base = reinterpret_cast<const uint8_t*>(0x1FFF7A10);
-        return infra::ConstByteRange(base, base + 12);
-    }
-#endif
-
-#if defined(STM32F7)
-    infra::ConstByteRange UniqueDeviceId()
-    {
-        const uint8_t* base = reinterpret_cast<const uint8_t*>(0x1FF0F420);
-        return infra::ConstByteRange(base, base + 12);
-    }
-#endif
-
-#if defined(STM32WB55xx)
-    infra::ConstByteRange UniqueDeviceId()
-    {
-        const uint8_t* base = reinterpret_cast<const uint8_t*>(0x1FFF7590);
-        return infra::ConstByteRange(base, base + 12);
-    }
-#endif
 }
