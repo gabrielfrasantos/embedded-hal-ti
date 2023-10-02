4299b51111154c4d9d72d61bee1282a2eb3dcfb6
Gabriel Santos <gabriel.fra.santos@gmail.com>
2023-10-02 09:16:54 +0200

    examples updated to use a middlelayer

--- examples/terminal_and_peripherals/Main.cpp
+++ examples/terminal_and_peripherals/Main.cpp
@@ -1,23 +1,19 @@
 #include "hal_tiva/instantiations/EventInfrastructure.hpp"
+#include "hal_tiva/instantiations/EventInfrastructure.hpp"
 #include "hal_tiva/instantiations/LaunchPadBsp.hpp"
 #include "hal_tiva/instantiations/TracerInfrastructure.hpp"
 #include "services/util/DebugLed.hpp"
-#include "hal_tiva/tiva/Clock.hpp"
 
 int main()
 {
-    hal::tiva::ConfigureClock(hal::tiva::crystalFrequency::_16_MHz, hal::tiva::oscillatorSource::main);
-
+    static instantiations::LaunchPad launchPad;
     static instantiations::EventInfrastructure eventInfrastructure;
-    static instantiations::LaunchPadUi ui;
     static instantiations::LaunchPadTerminalAndTracer tracer;
-    static services::DebugLed debugLed(ui.ledGreen, std::chrono::milliseconds(100), std::chrono::milliseconds(1400));
+    static services::DebugLed debugLed{ launchPad.DebugLed(), std::chrono::milliseconds(100), std::chrono::milliseconds(1400) };
 
     tracer.tracer.Trace() << "------------";
     tracer.tracer.Trace() << "|   TEST   |";
     tracer.tracer.Trace() << "------------";
-
-    //static services::TerminalWithCommandsImpl();
 
     eventInfrastructure.Run();
     __builtin_unreachable();
